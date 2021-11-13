beac3dbabb9648c2069735972832788b8407f66c
Soumyadigital <soumyadigital22@gmail.com>
2021-11-13 09:02:05 +0530

    Initial Commit

---
+++ db.js
@@ -0,0 +1,19 @@
+function dbConnect() {
+    // Db connection
+const mongoose = require('mongoose')
+const url = 'mongodb://localhost/comments'
+
+mongoose.connect(url, {
+    useUnifiedTopology: true,
+    useFindAndModify: true
+})
+
+const connection = mongoose.connection
+connection.once('open', function() {
+    console.log('Database connected...')
+}).catch(function(err){
+    console.log('Connection failed...')
+})
+}
+
+module.exports = dbConnect
